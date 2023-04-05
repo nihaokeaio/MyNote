346503ab9dab13ff6d9b80469c232266b7274f0e
zqd <617253864@qq.com>
2023-04-05 21:30:05 +0800

    Animation

--- demo11/widget.cpp
+++ demo11/widget.cpp
@@ -17,7 +17,7 @@ Widget::Widget(QWidget *parent) :
     hlay->addWidget(startcharts);
 
     //this->setWindowFlags(Qt::FramelessWindowHint);
-    ui->displayWidget->setBackgroundBrush(QBrush(Qt::red));
+    //ui->displayWidget->setBackgroundBrush(QBrush(Qt::red));
     connect(startcharts,&QPushButton::clicked,this,&Widget::slotStartcharts);
     connect(startAnimation,&QPushButton::clicked,this,&Widget::slotStartAnimation);
 
@@ -36,11 +36,13 @@ Widget::~Widget()
 void Widget::animationChange(QVariant value)
 {
     ++time;
-    datalist.append(QPointF(time,value.toInt()));
+    ;
+    datalist.append(QPointF(time,value.toRect().x()));
 }
 
 void Widget::slotStartcharts()
 {
+     ui->chartsWidget->clearMask();
     QSplineSeries* series=new QSplineSeries;
     series->append(datalist);
 
@@ -48,23 +50,34 @@ void Widget::slotStartcharts()
     chart->legend()->hide();
     chart->addSeries(series);
     chart->createDefaultAxes();
-    chart->axes(Qt::Vertical).first()->setRange(0,400);
+    chart->axes(Qt::Vertical).first()->setRange(0,1000);
     chart->axes(Qt::Horizontal).first()->setRange(0,time);
     ui->chartsWidget->setChart(chart);
+    datalist.clear();
+    time=0;
 }
 
 void Widget::slotStartAnimation()
 {
-    static int resizeValue{500};
-    resizeValue = -resizeValue;
+    static int resizeValue{ui->displayWidget->width()};
+
     animation = new QPropertyAnimation(ui->displayWidget, "geometry");
     animation->setDuration(500);
+    qDebug()<<QString("%1,%2,%3,%4").arg(ui->displayWidget->pos().x()).arg(ui->displayWidget->pos().y()).arg(ui->displayWidget->width()).arg(ui->displayWidget->height());
     animation->setStartValue(QRect(ui->displayWidget->pos().x(), ui->displayWidget->pos().y(), ui->displayWidget->width(), ui->displayWidget->height()));
-    animation->setEndValue(QRect(ui->displayWidget->pos().x() + resizeValue, ui->displayWidget->pos().y() + resizeValue, 0, ui->displayWidget->height()));
+
+    qDebug()<<ui->displayWidget->pos().x() + resizeValue;
+    qDebug()<<ui->displayWidget->width() - resizeValue;
+    int width_h=ui->displayWidget->width() - resizeValue;
+    qDebug()<<ui->displayWidget->width();
+    qDebug()<<+ resizeValue;
+
+    animation->setEndValue(QRect(ui->displayWidget->pos().x() + resizeValue, ui->displayWidget->pos().y(), width_h, ui->displayWidget->height()));
 //    //animation->setEndValue(QRect(pos().x() + width(), pos().y()+height(), 0, 0));
+    resizeValue = -resizeValue;
 //    animation->setStartValue(100);
 //    animation->setEndValue(400);
-    animation->setEasingCurve(QEasingCurve::Linear);
+    animation->setEasingCurve(QEasingCurve::InOutQuart);
     animation->start(QAbstractAnimation::DeleteWhenStopped);
     connect(animation,&QPropertyAnimation::valueChanged,this,&Widget::animationChange);
 }

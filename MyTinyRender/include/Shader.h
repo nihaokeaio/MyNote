03bf210c63c1ce46205576401fcd72c6d733ee57
zqd <617253864@qq.com>
2024-06-16 19:00:49 +0800

    修该文本编码 实现了omp并行绘制 创建了fragShaderFun

---
+++ MyTinyRender/include/Shader.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Mesh.h"
+#include "Texture.h"
+
+struct fragment_shader_payload
+{
+	fragment_shader_payload() = delete;
+
+
+	explicit fragment_shader_payload(const Vec3f& col, const Vec3f& nor, const Vec2f& tc, const Mesh* m) :
+		color(col), normal(nor), tex_coords(tc), mesh(m)
+	{
+	}
+
+	Vec3f view_pos;
+	Vec3f color;
+	Vec3f normal;
+	Vec2f tex_coords;
+	const Mesh* mesh = nullptr;
+};
+
+struct vertex_shader_payload
+{
+	Eigen::Vector3f position;
+};
